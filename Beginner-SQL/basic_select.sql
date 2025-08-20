ef6811b2c7e1a14f755bf4beffe455f6e36b3622
Priyash94Shukla <102786974+Priyash94Shukla@users.noreply.github.com>
2025-08-20 22:15:04 +0530

    Create basic_select.sql

---
+++ Beginner-SQL/basic_select.sql
@@ -0,0 +1,8 @@
+-- Select all data from Employees table
+SELECT * FROM Employees;
+
+-- Select only Name and Salary from Employees table
+SELECT Name, Salary FROM Employees;
+
+-- Select employees with Salary > 50000
+SELECT Name, Salary FROM Employees WHERE Salary > 50000;
